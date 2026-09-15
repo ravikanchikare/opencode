cb6c6425d4ada19cb5b987c9d4ee8d414c52d9dc
Ravi Kanchikare <krhebbar@gmail.com>
2026-09-15 07:54:26 +0530

    feat(plugin): expose exact-ID options and descriptive tool catalogs

--- packages/core/src/instance.ts
+++ packages/core/src/instance.ts
@@ -26,6 +26,7 @@ import { Permission } from "./permission.js"
 import { Plugin } from "./plugin.js"
 import { PluginHooks } from "./plugin/hooks.js"
 import { InstancePlugins } from "./plugin/instance.js"
+import { PluginOptionConfig } from "./plugin/option-config.js"
 import { PluginSupervisor } from "./plugin/supervisor.js"
 import { WorktreeStrategies } from "./worktree/strategies.js"
 import { Pty } from "./pty.js"
@@ -74,6 +75,7 @@ const nodes = [
   Plugin.node,
   PluginHooks.node,
   InstancePlugins.node,
+  PluginOptionConfig.node,
   PluginSupervisor.node,
   WorktreeStrategies.node,
   FileSystemSearch.node,
